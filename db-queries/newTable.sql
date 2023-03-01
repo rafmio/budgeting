016eb9b85fb1f93caf5385f3dc4f32c0d6054ecd
rafmio <rafmio@yandex.ru>
2023-03-01 11:40:26 +0300

    Work 01.03.2023 11:40

--- db-queries/newTable.sql
+++ db-queries/newTable.sql
@@ -7,15 +7,15 @@ CREATE DATABASE budgeting;
 CREATE TABLE transactions (
     transaction_id serial PRIMARY KEY,
     account VARCHAR (20),
-    trdate VARCHAR (10), -- date of transaction
-    trtype VARCHAR (32), -- type of transaction (tecnical or to counterpary)
-    docdate VARCHAR (10), -- date of document
+    trdate DATE, -- date of transaction
+    trtype DATE, -- type of transaction (tecnical or to counterpary)
+    docdate DATE, -- date of document
     docnumb VARCHAR (10), -- number of document
     counterpary VARCHAR (64),
     cntp_tax_id VARCHAR(12),
     cntp_contract VARCHAR (64),
     purpose VARCHAR (255), -- purpose of payment
-    comment VARCHAR (255), -- comment himself 
+    comnt VARCHAR (255), -- comment himself 
     direction VARCHAR (10), --inflow, outflow
     amount NUMERIC(2),
     item VARCHAR(64)
